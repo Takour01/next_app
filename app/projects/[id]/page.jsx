16c9ee3c020f59dd96001003ccfa5f55a24b44d9
Takour01 <azerpoi675@gmail.com>
2023-09-06 22:51:51 +0100

    sdsd

--- app/projects/[id]/page.jsx
+++ app/projects/[id]/page.jsx
@@ -1,5 +1,12 @@
-const Projects = ({ id }) => {
-  return <div>Projects {id}</div>;
+export async function generateStaticParams() {
+  const posts = [1, 2, 3];
+  return posts.map((post) => ({
+    id: post,
+  }));
+}
+
+const Project = ({ params }) => {
+  return <div>Project {params.id}</div>;
 };
 
-export default Projects;
+export default Project;
